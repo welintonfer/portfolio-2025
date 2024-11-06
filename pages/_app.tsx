798366390602c467a40e14ed612acc3c3896695b
Welinton Fernadnes <portfolio.fernandes@gmail.com>
2024-11-06 14:54:49 -0300

    Vercel style hidden 2

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,6 +1,6 @@
 // src/app/layout.tsx ou src/pages/_app.tsx
 
-import "./globals.css";
+import "../styles/globals.css";
 import ScrollComponent from "@/components/ScrollComponent";
 
 export default function RootLayout({ children }: { children: React.ReactNode }) {
